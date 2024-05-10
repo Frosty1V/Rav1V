9d0d5c648d99eb4849b97848c17f570b60ab6017
Frosty1V <147980724+Frosty1V@users.noreply.github.com>
2024-05-10 12:27:10 +0300

    Update وضع ترحيب.js

--- plugins/وضع ترحيب.js
+++ plugins/وضع ترحيب.js
@@ -1,13 +1,11 @@
-const handler = async (m, {conn, text, isROwner, isOwner}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.gc_setwelcome
+//import db from '../lib/database.js'
 
+let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
+    global.db.data.chats[m.chat].sWelcome = text
     m.reply('*تــم وضــع رســالــة الــتـرحـيب !*')
   } else throw `*أدخــل رســالــة الـتـرحــيب !*\n*عــشان تــعـمل مـنــشن أكــتب @user, عــشان تـحط أســم الـجروب أكـتب @group, عــشان تـحط وصــف أكــتب @desc*`
-};
+}
 handler.help = ['setwelcome <text>']
 handler.tags = ['group']
 handler.command = ['الترحيب'] 
