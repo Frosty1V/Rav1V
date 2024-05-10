87176742be821ccb32c3c4f43952485408402b82
Frosty1V <mn6702488@gmail.com>
2024-05-10 12:31:28 +0300

    ّ

--- plugins/وضع ترحيب.js
+++ plugins/وضع ترحيب.js
@@ -1,17 +1,17 @@
+import _translate from "./_translate.js"
+const tradutor = _translate.plugins.gc_setwelcome
+// Para configurar o idioma, na raiz do projeto altere o arquivo config.json
+// Para configurar el idioma, en la raíz del proyecto, modifique el archivo config.json.
+// To set the language, in the root of the project, modify the config.json file.
+
 const handler = async (m, {conn, text, isROwner, isOwner}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.gc_setwelcome
-
   if (text) {
-    m.reply('*تــم وضــع رســالــة الــتـرحـيب !*')
-  } else throw `*أدخــل رســالــة الـتـرحــيب !*\n*عــشان تــعـمل مـنــشن أكــتب @user, عــشان تـحط أســم الـجروب أكـتب @group, عــشان تـحط وصــف أكــتب @desc*`
+    global.db.data.chats[m.chat].sWelcome = text;
+    m.reply(tradutor.texto1);
+  } else throw `${tradutor.texto2[0]}\n*- @user (mención)*\n*- @group (nombre de grupo)*\n*- @desc (description de grupo)*`;
 };
-handler.help = ['setwelcome <text>']
-handler.tags = ['group']
-handler.command = ['الترحيب'] 
-handler.admin = true
-handler.owner = false
-
-export default handler
+handler.help = ['setwelcome <text>'];
+handler.tags = ['group'];
+handler.command = ['setwelcome'];
+handler.admin = true;
+export default handler;
