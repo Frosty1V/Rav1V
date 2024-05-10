f22c1e7cdaf8728734b244462f53626d785308c7
Frosty1V <147980724+Frosty1V@users.noreply.github.com>
2024-05-10 10:04:34 +0300

    Update حقوق.js

--- plugins/حقوق.js
+++ plugins/حقوق.js
@@ -1,30 +1,23 @@
-import {addExif} from '../lib/sticker.js';
-
-const handler = async (m, {conn, text}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.sticker_wm
-
-  if (!m.quoted) throw tradutor.texto1;
-  let stiker = false;
-  try {
-    let [packname, ...author] = text.split('|');
-    author = (author || []).join('|');
-    const mime = m.quoted.mimetype || '';
-    if (!/webp/.test(mime)) throw tradutor.texto2;
-    const img = await m.quoted.download();
-    if (!img) throw tradutor.texto3;
-    stiker = await addExif(img, packname || global.packname, author || global.author);
-  } catch (e) {
-    console.error(e);
-    if (Buffer.isBuffer(e)) stiker = e;
-  } finally {
-    if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, {asSticker: true});
-    else throw tradutor.texto3;
-  }
-};
-handler.help = ['wm <packname>|<author>'];
-handler.tags = ['sticker'];
-handler.command = /^سرقة|حقوق/i;
-export default handler;
+import { addExif } from '../lib/sticker.js'
+let handler = async (m, { conn, text }) => {
+if (!m.quoted) throw 'الرجاء الاشارة الى الملصق الذي تريد أن تغير حقوقه*'
+let stiker = false
+try {
+let [packname, ...author] = text.split('|')
+author = (author || []).join('|')
+let mime = m.quoted.mimetype || ''
+if (!/webp/.test(mime)) throw '*الرجاء الاشارة الى الملصق الذي تريد أن تغير حقوقه *'
+let img = await m.quoted.download()
+if (!img) throw '*الرجاء  الاشارة الى الملصق الذي تريد أن تغير حقوقه*'
+stiker = await addExif(img, packname || global.packname, author || global.author)
+} catch (e) {
+console.error(e)
+if (Buffer.isBuffer(e)) stiker = e
+} finally {
+if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, { asSticker: true })
+else throw '*قم باستخدام الامر مرة اخرى هناك مشكله*'
+}}
+handler.help = ['wm']
+handler.tags = ['sticker']
+handler.command = /^حقوق|سرقة$/i
+export default handler
