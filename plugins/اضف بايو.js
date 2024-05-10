e5eeb2b8bb5e2971f7fc6fd7b708deddd6a2ce5b
Frosty1V <147980724+Frosty1V@users.noreply.github.com>
2024-05-10 11:48:40 +0300

    Update اضف بايو.js

--- plugins/اضف بايو.js
+++ plugins/اضف بايو.js
@@ -1,15 +1,19 @@
 let handler = async (m, { conn, text }) => {
-  if (!text) throw `هذا الامر يمكنك من خلاله ان تغير بايو البوت \n*غير_البايو صل على النبي.*`
+    if (!text) throw `لم يتم تحديد نص. يرجى إعادة المحاولة بتحديد النص الجديد للـ Bio.\nمثال: ${usedPrefix}تغيير البايو صل على النبي.`
     try {
-   await conn.updateProfileStatus(text).catch(_ => _)
-   conn.reply(m.chat, ' تم تغيير البايو بنجاح', m)
-} catch {
-      throw 'Yah Error.. :D'
+        let command = text.split(' ')[0].toLowerCase() // يأخذ الجزء الأول من النص كـ command
+        let newBio = text.substring(command.length + 1) // يأخذ الجزء الباقي من النص كـ الـ Bio
+
+        await conn.setStatus(newBio).catch(_ => _)
+        conn.reply(m.chat, 'تم تحديث الـ Bio بنجاح.', m)
+    } catch {
+        throw 'حدث خطأ أثناء تحديث الـ Bio.'
     }
 }
-handler.help = ['setbio']
+
+handler.help = ['تغيير البايو']
 handler.tags = ['owner']
-handler.command = /^(تغيير_البايو)$/i
+handler.command = /^(تغيير البايو)$/i
 handler.owner = true
 
 export default handler
