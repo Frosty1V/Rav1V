68468567265f468e30b2428d31ea92b1d3fc6da0
Frosty1V <147980724+Frosty1V@users.noreply.github.com>
2024-05-11 17:12:01 +0300

    Update المناخ او الطقس.js

--- plugins/المناخ او الطقس.js
+++ plugins/المناخ او الطقس.js
@@ -1,22 +1,30 @@
-import axios from "axios"
+import axios from "axios";
+
 let handler = async (m, { args }) => {
-if (!args[0]) throw "*اكتب اسم المدينة او البلد الذي تريد ان تعرف مناخه*"
-try {
-const response = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${args}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273`)
-const res = await response
-const name = res.data.name
-const Country = res.data.sys.country
-const Weather = res.data.weather[0].description
-const Temperature = res.data.main.temp + "°C"
-const Minimum_Temperature = res.data.main.temp_min + "°C"
-const Maximum_Temperature = res.data.main.temp_max + "°C"
-const Humidity = res.data.main.humidity + "%"
-const Wind = res.data.wind.speed + "km/h"
-const wea = `「 📍 」المكان: ${name}\n「 🗺️ 」البلد: ${Country}\n「 🌤️ 」المنظر: ${Weather}\n「 🌡️ 」درجة الحراره: ${Temperature}\n「 💠 」 درجة الحراره المئاوية : ${Minimum_Temperature}\n「 📛 」 MAXIMUM TEMPERATURE: ${Maximum_Temperature}\n「 💦 」HUMIDITY: ${Humidity}\n「 🌬️ 」 WINDSPEED: ${Wind}`
-m.reply(wea)
-} catch {
-return "*ERROR*"}}
-handler.help = ['climate']
-handler.tags = ['tools']
-handler.command = /^(المناخ|الطقس)$/i
-export default handler
+    if (!args[0]) throw "*الرجاء كتابة اسم المدينة أو البلد الذي تريد معرفة مناخه*";
+    try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${args}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273`);
+        const res = response.data;
+
+        const name = res.name;
+        const country = res.sys.country;
+        const weatherDesc = res.weather[0].description;
+        const temperature = res.main.temp + "°C";
+        const minTemp = res.main.temp_min + "°C";
+        const maxTemp = res.main.temp_max + "°C";
+        const humidity = res.main.humidity + "%";
+        const windSpeed = res.wind.speed + "km/h";
+
+        const weatherInfo = `「 📍 」المكان: ${name}\n「 🗺️ 」البلد: ${country}\n「 🌤️ 」المنظر: ${weatherDesc}\n「 🌡️ 」درجة الحرارة: ${temperature}\n「 💠 」 درجة الحرارة الصغرى: ${minTemp}\n「 📛 」 درجة الحرارة العظمى: ${maxTemp}\n「 💦 」الرطوبة: ${humidity}\n「 🌬️ 」سرعة الرياح: ${windSpeed}`;
+
+        m.reply(weatherInfo);
+    } catch (error) {
+        return "*خطأ في الاستعلام عن الطقس*";
+    }
+};
+
+handler.help = ['الطقس'];
+handler.tags = ['أدوات'];
+handler.command = /^(المناخ|الطقس)$/i;
+
+export default handler;
